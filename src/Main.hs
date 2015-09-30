12ce4862303111b95702ef49c58463db8056e3eb
Erik Deijl <erik.deijl@gmail.com>
2015-09-30 16:19:12 +0200

    easy sample program with an FFI

---
+++ src/Main.hs
@@ -0,0 +1,15 @@
+{-# LANGUAGE ForeignFunctionInterface #-}
+module Main where
+import Control.Concurrent
+import Control.Monad
+
+
+foreign export ccall startCounter :: Int -> IO ()
+startCounter :: Int -> IO ()
+startCounter = void . forkIO . void . loop
+  where loop i = do
+        putStrLn (replicate i 'o')
+        threadDelay (10^6)
+        loop (i + 1)
+
+
