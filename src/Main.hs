88eb2c0a55249dbbc8367a8613695fc484f72bc9
Erik Deijl <erik.deijl@gmail.com>
2015-10-01 08:45:18 +0200

    Demo function that prints a triangle of 'o' characters in the console

--- src/Main.hs
+++ src/Main.hs
@@ -3,13 +3,10 @@ module Main where
 import Control.Concurrent
 import Control.Monad
 
-
 foreign export ccall startCounter :: Int -> IO ()
 startCounter :: Int -> IO ()
 startCounter = void . forkIO . void . loop
   where loop i = do
-        putStrLn (replicate i 'o')
-        threadDelay (10^6)
-        loop (i + 1)
-
-
+    putStrLn (replicate i 'o')
+    threadDelay (10^6)
+    loop (i + 1)
