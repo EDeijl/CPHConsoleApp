a2d837f47669a6a36dfd70fa6c16ffee2ae689db
Erik Deijl <erik.deijl@gmail.com>
2015-10-06 10:23:57 +0200

    no more android project, init.c corresponds with project

--- src/init.c
+++ src/init.c
@@ -2,14 +2,14 @@
 #include <HsFFI.h>
 
 extern void hs_set_java_vm(HSPtr a1);
-extern void __stginit_HaskellActivity(void);
+extern void __stginit_Counter(void);
 
-JNIEXPORT jint JNICALL JNI_OnLoad( JavaVM *vm, void *pvt ) {
-  static char *argv[] = { "libhaskell.so", 0}, **argv_ =argv;
+JNIEXPORT jint JNICALL JNI_HelloWorld( JavaVM *vm, void *pvt ) {
+  static char *argv[] = { "CPConsoleAppStaticLib", 0}, **argv_ =argv;
   static int argc = 1;
 
   hs_init(&argc, &argv_);
-  hs_add_root(__stginit_HaskellActivity);
+  hs_add_root(__stginit_Counter);
   hs_set_java_vm(vm);
   return JNI_VERSION_1_2;
 }
