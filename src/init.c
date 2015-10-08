3b1cd13715940f76ca842eb18698f4b662e4b5db
Erik Deijl <erik.deijl@gmail.com>
2015-10-08 16:10:42 +0200

    reset to static lib version, got further this way

--- src/init.c
+++ src/init.c
@@ -2,14 +2,14 @@
 #include <HsFFI.h>
 
 extern void hs_set_java_vm(HsPtr a1);
-extern void __stginit_Counter(void);
+extern void __stginit_HaskellActivity(void);
 
 JNIEXPORT jint JNICALL JNI_OnLoad( JavaVM *vm, void *pvt ) {
   static char *argv[] = { "hw-prebuilt.so", 0}, **argv_ =argv;
   static int argc = 1;
 
   hs_init(&argc, &argv_);
-  hs_add_root(__stginit_Counter);
+  hs_add_root(__stginit_HaskellActivity);
   hs_set_java_vm(vm);
   return JNI_VERSION_1_2;
 }
