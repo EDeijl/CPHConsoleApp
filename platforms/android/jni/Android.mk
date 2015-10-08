ef298737152b8987a12abfcfb6e10e523774d822
Erik Deijl <erik.deijl@gmail.com>
2015-10-08 17:05:24 +0200

    changed make to static libraries

--- platforms/android/jni/Android.mk
+++ platforms/android/jni/Android.mk
@@ -3,6 +3,5 @@ LOCAL_PATH := $(call my-dir)
 include $(CLEAR_VARS)
 LOCAL_MODULE := hw-prebuilt
 
-LOCAL_STATIC_LIBRARIES := ../obj/local/$(TARGET_ARCH_ABI)/build/libHSCPConsoleApp-0.1.0.0.a
-include $(PREBUILT_SHARED_LIBRARY)
-include $(BUILD_SHARED_LIBRARY)
+LOCAL_SRC_FILES := ../obj/local/$(TARGET_ARCH_ABI)/build/libHSCPConsoleApp-0.1.0.0.a
+include $(PREBUILD_STATIC_LIBRARY)
