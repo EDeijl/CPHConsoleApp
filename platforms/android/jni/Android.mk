0d7499a31fc51261ccb0e4c6be17331654b14a83
Erik Deijl <erik.deijl@gmail.com>
2015-10-09 09:37:55 +0000

    makefile changes

--- platforms/android/jni/Android.mk
+++ platforms/android/jni/Android.mk
@@ -4,4 +4,4 @@ include $(CLEAR_VARS)
 LOCAL_MODULE := hw-prebuilt
 
 LOCAL_SRC_FILES := ../obj/local/$(TARGET_ARCH_ABI)/build/libHSCPConsoleApp-0.1.0.0.a
-include $(PREBUILD_STATIC_LIBRARY)
+include $(PREBUILT_STATIC_LIBRARY)
