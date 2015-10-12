7e5a206ca59d209ad351830c14de581939707abf
Erik Deijl <erik.deijl@gmail.com>
2015-10-12 11:39:04 +0200

    sed now terminates

--- build-android.sh
+++ build-android.sh
@@ -8,9 +8,9 @@ cp CPConsoleApp.cabal platforms/android/
 cp src/* platforms/android/src/
 
 cd platforms/android
-sed -i -e 's/-optl-shared/-shared' CPConsoleApp.cabal
-sed -i -e 's/cc-options/--cc-options' CPConsoleApp.cabal
-sed -i -e 's/ld-options/--ld-options' CPConsoleApp.cabal
+sed -i -e 's/-optl-shared/-shared/g' CPConsoleApp.cabal
+sed -i -e 's/cc-options/--cc-options/g' CPConsoleApp.cabal
+sed -i -e 's/ld-options/--ld-options/g' CPConsoleApp.cabal
 ant debug
 git reset --hard CPConsoleApp.cabal
 ant debug
