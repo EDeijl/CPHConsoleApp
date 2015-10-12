51a20b413656f0bb66d10509bcffccfa51cd40e9
Erik Deijl <erik.deijl@gmail.com>
2015-10-12 11:35:49 +0200

    Android build script now applies the cabal fix

--- build-android.sh
+++ build-android.sh
@@ -8,4 +8,9 @@ cp CPConsoleApp.cabal platforms/android/
 cp src/* platforms/android/src/
 
 cd platforms/android
+sed -i -e 's/-optl-shared/-shared' CPConsoleApp.cabal
+sed -i -e 's/cc-options/--cc-options' CPConsoleApp.cabal
+sed -i -e 's/ld-options/--ld-options' CPConsoleApp.cabal
+ant debug
+git reset --hard CPConsoleApp.cabal
 ant debug
