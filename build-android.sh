abdaa638fcc496b200dbc44ffab48f858cfc8751
Erik Deijl <erik.deijl@gmail.com>
2015-10-12 16:17:20 +0200

    typo in build script

--- build-android.sh
+++ build-android.sh
@@ -8,7 +8,7 @@ cp CPConsoleApp.cabal platforms/android/
 cp LICENSE platforms/android/
 cp src/* platforms/android/src/
 
-Cd platforms/android
+cd platforms/android
 sed -i -e '48s/-optl-shared/-shared/g' CPConsoleApp.cabal
 sed -i -e '52s/cc-options/--cc-options/g' CPConsoleApp.cabal
 sed -i -e '53s/ld-options/--ld-options/g' CPConsoleApp.cabal
